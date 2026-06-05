2dd7c3d6e6eee2419ba53cfff36318c7e2a75376
Jhkpop <drannemery@gmail.com>
2026-06-04 19:10:31 -0700

    Inserted investor before and after.
    Made it so you can add investor data. Also updated the database to auto increment table IDs. Plus, testconnection screenshot.

--- CS420_Assignment5_TableCreation.sql
+++ CS420_Assignment5_TableCreation.sql
@@ -1,19 +1,19 @@
 CREATE TABLE Investor(
-InvestorID int primary key NOT NULL,
+InvestorID int AUTO_INCREMENT primary key NOT NULL,
 FirstName varchar(50),
 LastName varchar(50),
 Email varchar(50),
 Phone varchar(13)
 );
 CREATE TABLE Company(
-CompanyID int primary key NOT NULL,
+CompanyID int AUTO_INCREMENT primary key NOT NULL,
 Industry varchar(50) NOT NULL,
 Headquarters varchar(50),
 FoundedYear year
 );
 
 CREATE TABLE BrokerageAccount(
-AccountID int primary key NOT NULL,
+AccountID int AUTO_INCREMENT primary key NOT NULL,
 AccountType varchar(50) NOT NULL,
 Balance decimal(10, 2),
 InvestorID int NOT NULL,
@@ -21,17 +21,17 @@ CONSTRAINT Investor_ID FOREIGN KEY (InvestorID) REFERENCES Investor(InvestorID)
 );
 
 CREATE TABLE Stock(
-StockID int primary key NOT NULL,
+StockID int AUTO_INCREMENT primary key NOT NULL,
 TickerSymbol varchar(50),
 ExchangeName varchar(50),
 CurrentPrice decimal(10, 2),
 CompanyID int NOT NULL,
-CONSTRAINT Company_ID FOREIGN KEY (Company) REFERENCES Company(CompanyID)
+CONSTRAINT Company_ID FOREIGN KEY (CompanyID) REFERENCES Company(CompanyID)
 );
 
 
 CREATE TABLE TradeTransaction(
-TransactionID int primary key NOT NULL,
+TransactionID int AUTO_INCREMENT primary key NOT NULL,
 TradeDate date NOT NULL,
 TradeType varchar(50),
 Quantity int,
@@ -39,5 +39,10 @@ PricePerShare decimal(10, 2),
 AccountID int NOT NULL,
 CONSTRAINT Account_ID FOREIGN KEY (AccountID) REFERENCES BrokerageAccount(AccountID),
 StockID int NOT NULL,
-CONSTRAINT Stock_ID FOREIGN KEY (Stock) REFERENCES Stock(StockID)
+CONSTRAINT Stock_ID FOREIGN KEY (StockID) REFERENCES Stock(StockID)
 );
+DROP TABLE Investor;
+DROP TABLE Company;
+DROP TABLE Stock;
+DROP TABLE BrokerageAccount;
+DROP TABLE TradeTransaction;
